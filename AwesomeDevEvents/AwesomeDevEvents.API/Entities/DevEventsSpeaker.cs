84cc99889836275c1e166a7ce0d80b6c5aac0725
Nascimento Johnny <jhonny_andersom@hotmail.com>
2023-06-02 22:11:49 -0300

    Created project and RESTful API endpoints without a database.

---
+++ AwesomeDevEvents/AwesomeDevEvents.API/Entities/DevEventsSpeaker.cs
@@ -0,0 +1,10 @@
+﻿namespace AwesomeDevEvents.API.Entities
+{
+    public class DevEventsSpeaker
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? TalkTitle { get; set; }
+        public string? LinkedInProfile { get; set; }
+    }
+}
